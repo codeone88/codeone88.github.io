f65fb25c4de06a98a8c79077e2939d7ac438662e
codeone88 <56555546+codeone88@users.noreply.github.com>
2019-10-14 14:12:50 -0500

    added widgets test

---
+++ Scripts/main.js
@@ -0,0 +1,42 @@
+
+window.addEventListener("load", function() { 
+   document.body.style.width='100%';
+   document.body.style.height='100%';
+}, false);
+
+
+
+function init(){
+	loadWidgets();
+}
+
+function loadWidgets(){
+	var data_url = "widgets.json";
+	var data_obj = JSON.parse(get_data_from_url(data_url));
+	var arrWid = data_obj.data;
+	
+	condole.log(arrWid[0].name);
+	
+	for(var i=0; i<arrWid.length; i++){
+		var _box = document.createElement('div'),
+			_name = document.createElement('div'),
+			_img = document.createElement('img');
+			
+		_box.classList.add('wid-box');
+		_box.classList.add('midX');
+			
+		_img.src = 'img/widgets/' + arrWid[i].img;
+		_img.classList.add('im-wid');
+		_box.appendChild(_img);
+		
+		document.getElementById('wid-cont').appendChild(_box);
+	}
+}
+
+
+function get_data_from_url(url){
+    var http_req = new XMLHttpRequest();
+    http_req.open("GET",url,false);
+    http_req.send(null);
+    return http_req.responseText;          
+}
