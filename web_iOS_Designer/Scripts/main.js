7276ef3865609ec56a2b75be9e1922af301c82d1
codeone88 <56555546+codeone88@users.noreply.github.com>
2020-01-07 16:31:10 -0500

    added themes

--- web_iOS_Designer/Scripts/main.js
+++ web_iOS_Designer/Scripts/main.js
@@ -6,7 +6,7 @@ window.addEventListener("load", function() {
 
 
 
-function init(){
+function initThemes(){
 	AOS.init({
         easing: 'ease-in-out-sine'
     });
@@ -14,7 +14,7 @@ function init(){
 	const menuItems = document.querySelectorAll('.txt-head');
 	menuItems.forEach(item => item.addEventListener('click', openWindow));
 	
-	//loadWidgets();
+	loadThemes();
 }
 
 function goHome(){
@@ -23,7 +23,7 @@ function goHome(){
 	}, 200);
 }
 
-function openWindow(e){
+/*function openWindow(e){
 	
 	$('input[name=menu-btn]').attr('checked', false);
 	
@@ -46,12 +46,90 @@ function openWindow(e){
 		}, 200);
 	}
 	
-	/*if(this.id === 'widBtn'){
-		$('html, body').animate({
-			scrollTop: $('#section3').offset().top
-		}, 200);
+}*/
+
+
+//--------------------- LOAD THEMES ---------------------------
+var arrSet = [];
+function loadThemes(){
+	var data_url = "themes.json";
+	var data_obj = JSON.parse(get_data_from_url(data_url));
+	arrSet = data_obj.data;
+	
+	themeMaker.makeTheme({
+        holder: document.getElementById('section2T'),
+		color: '#484848',
+        dayCount: arrSet.length, //number of days
+        dayWidth: 300, //holder width can also be used as spacing
+        dayHeight: 250, //holder height
+        imgWidth: 300, //image size
+        font: 'reg',
+        dayTop: 210, //day vertical position from top
+        tempBottom: 0, //temp vertical position from bottom
+        fontSize: 20, //day text and temp size
+        iconURL: 'img/themes/', //url to icon
+        iconsName: 'weatherIcons', //id for weather icons (increments)
+        daysName: 'weatherDays', //id for days (increments)
+        tempsName: 'weatherTemps', //id for temps (increments)
+    });
+	
+	var icon, i;
+    for (i = 0; i < fcastObj.dayCount; i++) { 
+        document.getElementById(fcastObj.icon + i).src = fcastObj.iconURL +  arrSet[i].img[0];
+        document.getElementById(fcastObj.day + i).innerHTML = arrSet[i].name;
+        document.getElementById(fcastObj.temp + i).innerHTML = arrSet[i].price;
+        document.getElementById(fcastObj.mmv + i).innerHTML = 'iOS ' + arrSet[i].miniosver + ' - ' + arrSet[i].maxiosver;
+    }
+	
+	/*for(var i=0; i<arrSet.length; i++){
+		var _box = document.createElement('div'),
+			_name = document.createElement('div'),
+			_mmver = document.createElement('div'),
+			_price = document.createElement('div'),
+			_img = document.createElement('img');
+			
+		_box.classList.add('set-box');
+		//_box.classList.add('midX');
+		
+		_img.src = 'img/setups/' + arrSet[i].img;
+		_img.classList.add('im-set');
+		_box.appendChild(_img);
+		
+		_name.innerHTML = arrSet[i].name;
+		_name.classList.add('center');
+		_name.classList.add('nameS');
+		_box.appendChild(_name);
+		
+		_box.classList.add('set-item');
+		_box.setAttribute("id", i);
+		
+		document.getElementById('set-cont').appendChild(_box);
 	}
-	*/
+	
+	/*const locItems = document.querySelectorAll('.set-item');
+	const imItems = document.querySelectorAll('.im-set');
+	locItems.forEach(item => item.addEventListener('click', openSetup));
+	
+	imItems.forEach(item => item.addEventListener('load', function(){
+		this.style.opacity = 1;
+	}));*/
+}
+
+/*function openSetup(){
+	document.getElementById('setup-viewer').style.display = 'block';
+	document.getElementById('full-set').src = 'img/setups/' + arrSet[this.id].img;
+	document.getElementById('_desc').innerHTML = '<b>' + arrSet[this.id].name + '</b><br><br>' +
+									arrSet[this.id].list + '<br><br>' +
+									"by <a href='" + arrSet[this.id].user + "' target='_blank'>" + arrSet[this.id].username + '</a>';
+}*/
+
+
+//--------------------------- JSON FUNCTION ------------------------------
+function get_data_from_url(url){
+    var http_req = new XMLHttpRequest();
+    http_req.open("GET",url,false);
+    http_req.send(null);
+    return http_req.responseText;        
 }
 
 
@@ -108,57 +186,4 @@ function openSubmit(){
 }
 
 
-//--------------------- LOAD SETUPS ---------------------------
-var arrSet = [];
-function loadSetups(){
-	var data_url = "setups.json";
-	var data_obj = JSON.parse(get_data_from_url(data_url));
-	arrSet = data_obj.data;
-	
-	for(var i=0; i<arrSet.length; i++){
-		var _box = document.createElement('div'),
-			_name = document.createElement('div'),
-			_img = document.createElement('img');
-			
-		_box.classList.add('set-box');
-		//_box.classList.add('midX');
-		
-		_img.src = 'img/setups/' + arrSet[i].img;
-		_img.classList.add('im-set');
-		_box.appendChild(_img);
-		
-		_name.innerHTML = arrSet[i].name;
-		_name.classList.add('center');
-		_name.classList.add('nameS');
-		_box.appendChild(_name);
-		
-		_box.classList.add('set-item');
-		_box.setAttribute("id", i);
-		
-		document.getElementById('set-cont').appendChild(_box);
-	}
-	
-	const locItems = document.querySelectorAll('.set-item');
-	const imItems = document.querySelectorAll('.im-set');
-	locItems.forEach(item => item.addEventListener('click', openSetup));
-	
-	imItems.forEach(item => item.addEventListener('load', function(){
-		this.style.opacity = 1;
-	}));
-}
-
-function openSetup(){
-	document.getElementById('setup-viewer').style.display = 'block';
-	document.getElementById('full-set').src = 'img/setups/' + arrSet[this.id].img;
-	document.getElementById('_desc').innerHTML = '<b>' + arrSet[this.id].name + '</b><br><br>' +
-									arrSet[this.id].list + '<br><br>' +
-									"by <a href='" + arrSet[this.id].user + "' target='_blank'>" + arrSet[this.id].username + '</a>';
-}
-
-
-function get_data_from_url(url){
-    var http_req = new XMLHttpRequest();
-    http_req.open("GET",url,false);
-    http_req.send(null);
-    return http_req.responseText;          
-}*/
+*/
