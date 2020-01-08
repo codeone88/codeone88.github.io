1a09e566ab54bb35693c39893ae83990d80e0d0a
codeone88 <56555546+codeone88@users.noreply.github.com>
2020-01-08 11:35:45 -0500

    Update main_theme.js

--- web_iOS_Designer/Scripts/main_theme.js
+++ web_iOS_Designer/Scripts/main_theme.js
@@ -73,7 +73,7 @@ function showSc(_src){
 		image.id = _src[i];
 		image.classList.add('sc');
 		imageimage.src = scPath + _src[i];
-		
+		console.log(scPath + _src[i]);
 		document.getElementById('scCont').appendChild(image);
 	}
 }
