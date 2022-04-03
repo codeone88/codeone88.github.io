fefbde910092b280e022276b13260f2bc3935cc3
codeone88 <56555546+codeone88@users.noreply.github.com>
2022-04-03 10:19:07 -0500

    more redirect

---
+++ Wallers/main-si.js
@@ -0,0 +1,17 @@
+
+window.addEventListener("load", function() { 
+   document.body.style.width='100%';
+   document.body.style.height='100%';
+}, false);
+
+var option;
+
+function init(){
+	
+}
+
+function openUrl(url){
+	window.open(url, '_blank').focus();
+}
+
+
